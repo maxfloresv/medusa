20a70c277909f6f2957efe7a69feb595191658e6
Máximo <45862114+Maximomfv@users.noreply.github.com>
2020-06-09 04:17:41 -0400

    Create temp-lista.js

---
+++ utils/temp-lista.js
@@ -0,0 +1,392 @@
+module.exports.games = [
+    {
+        "id": "Metal Gear Solid",
+        "info": "https://es.wikipedia.org/wiki/Metal_Gear_Solid",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719726984514633759/Metal-Gear-Solid.jpg"
+    },
+    {
+        "id": "Grand Theft Auto",
+        "info": "https://es.wikipedia.org/wiki/Grand_Theft_Auto",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719727529623289936/Grand-Theft-Auto.jpg"
+    },
+    {
+        "id": "Assassin's Creed",
+        "info": "https://es.wikipedia.org/wiki/Assassin%27s_Creed",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719727729578344458/Assassins-Creed.jpg"
+    },
+    {
+        "id": "The Last of Us",
+        "info": "https://es.wikipedia.org/wiki/The_Last_of_Us",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719728030381244436/The-Last-of-Us.jpg"
+    },
+    {
+        "id": "Resident Evil",
+        "info": "https://es.wikipedia.org/wiki/Resident_Evil",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719728425383886858/Resident-Evil.jpg"
+    },
+    {
+        "id": "Call of Duty",
+        "info": "https://es.wikipedia.org/wiki/Call_of_Duty",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719728612668080128/Call-of-Duty.jpg"
+    },
+    {
+        "id": "Batman",
+        "info": "https://es.wikipedia.org/wiki/Anexo:Videojuegos_de_Batman",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719728810798481478/Batman.jpg"
+    },
+    {
+        "id": "Persona",
+        "info": "https://es.wikipedia.org/wiki/Persona_(serie)",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719729272067194880/Persona.jpg"
+    },
+    {
+        "id": "Red Dead Redemption",
+        "info": "https://es.wikipedia.org/wiki/Red_Dead_Redemption",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719729630046715964/Red-Dead-Redemption.png"
+    },
+    {
+        "id": "Red Faction",
+        "info": "https://es.wikipedia.org/wiki/Red_Faction",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719729831318913044/Red-Faction.jpg"
+    },
+    {
+        "id": "FIFA",
+        "info": "https://es.wikipedia.org/wiki/FIFA_(serie)",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719730966486319225/FIFA.jpg"
+    },
+    {
+        "id": "Fallen Legion",
+        "info": "https://www.fallenlegiongame.com/",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719730035396706334/Fallen-Legion.jpg"
+    },
+    {
+        "id": "Just Dance",
+        "info": "https://es.wikipedia.org/wiki/Just_Dance_(serie)",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719730188509904916/Just-Dance.jpg"
+    },
+    {
+        "id": "Pro Evolution Soccer",
+        "info": "https://es.wikipedia.org/wiki/Pro_Evolution_Soccer_(serie)",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719730417938464788/Pro-Evolution-Soccer.jpg"
+    },
+    {
+        "id": "Shovel Knight",
+        "info": "https://es.wikipedia.org/wiki/Shovel_Knight",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719730642669273118/Shovel-Knight.jpg"
+    },
+    {
+        "id": "King's Quest",
+        "info": "https://es.wikipedia.org/wiki/King%27s_Quest",
+        "image": "https://cdn.discordapp.com/attachments/718996037250842645/719730825993912380/Kings-Quest.png"
+    },
+    {
+        "id": "Final Fantasy",
+        "info": "https://es.wikipedia.org/wiki/Final_Fantasy_(franquicia)",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719735429808652338/Final-Fantasy.jpg"
+    },    
+    {
+        "id": "Minecraft",
+        "info": "https://www.minecraft.net/es-es/",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719735835850702859/Minecraft.jpg"
+    },    
+    {
+        "id": "Skylanders",
+        "info": "https://www.skylanders.com/",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719736852122173440/Skylanders.jpg"
+    },    
+    {
+        "id": "Hopping Dinos",
+        "info": "https://es.wikipedia.org/wiki/HTTP_404",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719826463129337946/Hopping-Dinos.jpg"
+    },    
+    {
+        "id": "Ghost of Tsushima",
+        "info": "https://es.wikipedia.org/wiki/Ghost_of_Tsushima",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719737559848058930/Ghost-of-Tsushima.jpg"
+    },    
+    {
+        "id": "Hollow Knight",
+        "info": "https://es.wikipedia.org/wiki/Hollow_Knight",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719738646428778544/Hollow-Knight.jpg"
+    },
+    {
+        "id": "Cyberpunk 2077",
+        "info": "https://es.wikipedia.org/wiki/Cyberpunk_2077",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719739763414007898/cyberpunk-2077.jpg"
+    },    
+    {
+        "id": "Beyond Good and Evil",
+        "info": "https://es.wikipedia.org/wiki/Beyond_Good_%26_Evil",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719740988091793418/Beyond-Good-and-Evil.jpg"
+    },    
+    {
+        "id": "Dead Island",
+        "info": "https://es.wikipedia.org/wiki/Dead_Island",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719741459279904788/Dead-Island.jpg"
+    },    
+    {
+        "id": "Granblue Fantasy",
+        "info": "https://es.wikipedia.org/wiki/Granblue_Fantasy",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719741832346599464/Granblue-Fantasy.jpg"
+    },    
+    {
+        "id": "Marvel's Avengers",
+        "info": "https://es.wikipedia.org/wiki/Marvel%27s_Avengers",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719744028303949844/Marvels-Avengers.jpg"
+    },    
+    {
+        "id": "The Elder Scrolls",
+        "info": "https://es.wikipedia.org/wiki/The_Elder_Scrolls",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719744425034645574/The-Elder-Scrolls.jpg"
+    },    
+    {
+        "id": "Tales of Arise",
+        "info": "https://es.wikipedia.org/wiki/Tales_of_Arise",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719745367079387253/Tales-of-Arise.jpg"
+    },    
+    {
+        "id": "Fortnite",
+        "info": "https://www.epicgames.com/fortnite/es-ES/home",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719745846752837732/Fortnite.jpg"
+    },    
+    {
+        "id": "Apex Legends",
+        "info": "https://www.ea.com/es-es/games/apex-legends",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719746241797554236/Apex-Legends.jpg"
+    },    
+    {
+        "id": "Overcooked",
+        "info": "https://en.wikipedia.org/wiki/Overcooked",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719746565584977940/Overcooked.jpg"
+    },    
+    {
+        "id": "Dead or Alive",
+        "info": "https://es.wikipedia.org/wiki/Dead_or_Alive_(serie)",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719746919957528606/Dead-or-Alive.jpg"
+    },    
+    {
+        "id": "Gods and Monsters",
+        "info": "https://www.epicgames.com/store/en-US/product/gods-and-monsters/home",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719747425593720842/Gods-and-Monsters.jpg"
+    },    
+    {
+        "id": "Watch Dogs",
+        "info": "https://es.wikipedia.org/wiki/Watch_Dogs",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719747706104315985/Watch-Dogs.jpg"
+    },    
+    {
+        "id": "Dragon Age",
+        "info": "https://es.wikipedia.org/wiki/Dragon_Age",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719748034346614794/Dragon-Age.jpg"
+    },    
+    {
+        "id": "Dying Light",
+        "info": "https://es.wikipedia.org/wiki/Dying_Light",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719748633393627176/Dying-Light.jpg"
+    },    
+    {
+        "id": "Twin Mirror",
+        "info": "https://es.wikipedia.org/wiki/Twin_Mirror",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719749275881308180/Twin-Mirror.jpg"
+    },    
+    {
+        "id": "Crash Bandicoot",
+        "info": "https://es.wikipedia.org/wiki/Crash_Bandicoot_(videojuego)",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719749902372044848/Crash-Bandicoot.jpg"
+    },    
+    {
+        "id": "Visage",
+        "info": "https://es.wikipedia.org/wiki/Visage_(videojuego)",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719758537890463824/Visage.jpg"
+    },    
+    {
+        "id": "Psychonauts",
+        "info": "https://es.wikipedia.org/wiki/Psychonauts",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719809776464429056/Psychonauts.jpg"
+    },    
+    {
+        "id": "Animal Crossing",
+        "info": "https://es.wikipedia.org/wiki/Animal_Crossing_(serie)",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719811362213331034/Animal-Crossing.jpg"
+    },    
+    {
+        "id": "Bravely Default",
+        "info": "https://es.wikipedia.org/wiki/Bravely_Default",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719812074900815942/Bravely-Default.jpg"
+    },    
+    {
+        "id": "Xenoblade Chronicles",
+        "info": "https://es.wikipedia.org/wiki/Xenoblade_Chronicles",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719812668508078080/Xenoblade-Chronicles.jpg"
+    },    
+    {
+        "id": "Paper Mario",
+        "info": "https://es.wikipedia.org/wiki/Paper_Mario_(serie)",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719813198122844170/Paper-Mario.jpg"
+    },    
+    {
+        "id": "Super Mario World",
+        "info": "https://es.wikipedia.org/wiki/Super_Mario_World",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719813491363414016/Super-Mario-World.jpg"
+    },    
+    {
+        "id": "Super Smash Bros",
+        "info": "https://es.wikipedia.org/wiki/Super_Smash_Bros.",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719813772176261140/Super-Smash-Bros.jpg"
+    },    
+    {
+        "id": "Super Mario Bros",
+        "info": "https://es.wikipedia.org/wiki/Super_Mario_Bros.",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719814334234230865/Super-Mario-Bros.jpg"
+    },    
+    {
+        "id": "Super Mario Party",
+        "info": "https://es.wikipedia.org/wiki/Super_Mario_Party",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719814632864350218/Super-Mario-Party.jpg"
+    },    
+    {
+        "id": "The Legend of Zelda",
+        "info": "https://es.wikipedia.org/wiki/The_Legend_of_Zelda",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719814948720607282/The-Legend-of-Zelda.jpg"
+    },    
+    {
+        "id": "Thief Simulator",
+        "info": "https://store.steampowered.com/app/704850/Thief_Simulator/",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719815559981695046/Thief-Simulator.png"
+    },    
+    {
+        "id": "Uno",
+        "info": "https://es.wikipedia.org/wiki/Uno_(juego)",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719816232102002798/Uno.jpg"
+    },    
+    {
+        "id": "Monopoly",
+        "info": "https://es.wikipedia.org/wiki/Monopoly",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719816628396621844/Monopoly.jpg"
+    },    
+    {
+        "id": "Dragon Ball Xenoverse",
+        "info": "https://es.wikipedia.org/wiki/Dragon_Ball_Xenoverse",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719816880956899368/Dragon-Ball-Xenoverse.jpg"
+    },    
+    {
+        "id": "Super Mario Maker",
+        "info": "https://es.wikipedia.org/wiki/Super_Mario_Maker",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719817477491785768/Super-Mario-Maker.jpg"
+    },    
+    {
+        "id": "Tetris",
+        "info": "https://es.wikipedia.org/wiki/Tetris",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719817842886967386/Tetris.jpg"
+    },    
+    {
+        "id": "Dark Souls",
+        "info": "https://es.wikipedia.org/wiki/Dark_Souls",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719818146235678770/Dark-Souls.jpg"
+    },    
+    {
+        "id": "Pac-Man",
+        "info": "https://es.wikipedia.org/wiki/Pac-Man",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719818456249401444/Pac-Man.jpg"
+    },    
+    {
+        "id": "Kirby's Adventure",
+        "info": "https://es.wikipedia.org/wiki/Kirby%27s_Adventure",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719818964703641620/Kirbys-Adventure.png"
+    },    
+    {
+        "id": "Galaga",
+        "info": "https://es.wikipedia.org/wiki/Galaga",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719819196191473724/Galaga.jpg"
+    },    
+    {
+        "id": "Killer Instinct",
+        "info": "https://es.wikipedia.org/wiki/Killer_Instinct_(videojuego_de_1994)",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719819727920168999/Killer-Instinct.jpg"
+    },    
+    {
+        "id": "Space Invaders",
+        "info": "https://es.wikipedia.org/wiki/Space_Invaders",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719819979192533042/Space-Invaders.png"
+    },    
+    {
+        "id": "Diablo",
+        "info": "https://es.wikipedia.org/wiki/Diablo_(videojuego)",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719820558291959818/Diablo.jpg"
+    },    
+    {
+        "id": "Guitar Hero",
+        "info": "https://es.wikipedia.org/wiki/Guitar_Hero",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719821188347723816/Guitar-Hero.jpg"
+    },    
+    {
+        "id": "Clone Hero",
+        "info": "https://clonehero.net/",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719821933071302676/Clone-Hero.png"
+    },    
+    {
+        "id": "Arkanoid",
+        "info": "https://es.wikipedia.org/wiki/Arkanoid",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719822262693527562/Arkanoid.jpg"
+    },    
+    {
+        "id": "Super Contra",
+        "info": "https://es.wikipedia.org/wiki/Super_Contra",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719822634665115648/Super-Contra.jpg"
+    },    
+    {
+        "id": "World of Warcraft",
+        "info": "https://es.wikipedia.org/wiki/World_of_Warcraft",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719822909450747904/World-of-Warcraft.jpg"
+    },    
+    {
+        "id": "League of Legends",
+        "info": "https://es.wikipedia.org/wiki/League_of_Legends",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719823193162121226/League-of-Legends.jpg"
+    },    
+    {
+        "id": "Street Fighter",
+        "info": "https://es.wikipedia.org/wiki/Street_Fighter",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719823525946458202/Street-Fighter.png"
+    },    
+    {
+        "id": "The King of Fighters",
+        "info": "https://es.wikipedia.org/wiki/The_King_of_Fighters",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719824052860223548/The-King-of-Fighters.jpg"
+    },    
+    {
+        "id": "Mortal Kombat",
+        "info": "https://es.wikipedia.org/wiki/Mortal_Kombat_(serie)",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719824331508547624/Mortal-Kombat.jpg"
+    },    
+    {
+        "id": "Metal Slug",
+        "info": "https://es.wikipedia.org/wiki/Metal_Slug",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719824616637333564/Metal-Slug.jpg"
+    },    
+    {
+        "id": "Luigi's Mansion",
+        "info": "https://es.wikipedia.org/wiki/Luigi%27s_Mansion",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719824851044401172/Luigis-Mansion.jpg"
+    },    
+    {
+        "id": "Super Mario Odyssey",
+        "info": "https://es.wikipedia.org/wiki/Super_Mario_Odyssey",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719825098885824573/Super-Mario-Odyssey.png"
+    },    
+    {
+        "id": "Donkey Kong Country",
+        "info": "https://es.wikipedia.org/wiki/Donkey_Kong_Country",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719825361659101285/Donkey-Kong-Country.jpg"
+    },
+    {
+        "id": "Bomberman",
+        "info": "https://es.wikipedia.org/wiki/Bomberman",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719825997633159200/Bomberman.jpg"
+    },
+    {
+        "id": "Valorant", 
+        "info": "https://es.wikipedia.org/wiki/Valorant",
+        "image": "https://cdn.discordapp.com/attachments/719734231881875528/719826242836234280/Valorant.jpg"
+    }
+];
