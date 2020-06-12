11468acb0fc13d6e0e0418285e867775977aafd5
Aaron129Polocraft <aarongaymeryt12@gmail.com>
2020-06-12 00:35:37 -0400

    Actualización slap.js
    Añadidas imágenes y mensajes
    Falta: más frases y más imágenes

--- commands/slap.js
+++ commands/slap.js
@@ -5,8 +5,8 @@ module.exports.run = async (bot, message, args) => {
     let user = message.mentions.users.first() || (message.guild.members.cache.get(args[0]) !== undefined ? message.guild.members.cache.get(args[0]).user : false);
     if (!user) return new_error(message, "Ocurrió un error", "Debes mencionar a un usuario o especificar su ID."); // hay que cambiar esto por algo más original
 
-    const images = ["aquí va el link de la imagen 1", "aquí va el de la img 2"] // etc, etc, etc
-    const phrases = ["frase 1 cuando ocupas este comando", "frase 2", "frase 3"] // etc...
+    const images = ["https://cdn.discordapp.com/attachments/720857177107202079/720857497518342194/slap1.gif", "https://cdn.discordapp.com/attachments/720857177107202079/720857537141932142/slap2.gif", "https://cdn.discordapp.com/attachments/720857177107202079/720857869947371610/slap3.gif"] // etc, etc, etc
+    const phrases = ["{author} le ha metido una cachetada a {user}", "A {author} le pego una arrechera y le volteo la jeta a {user}"] // etc...
     const randomImg = images[Math.floor(Math.random() * images.length)];
     const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
 
