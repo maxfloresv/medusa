0b45dcf1423a3cf7345909d0b5f34fe2ffb70f6e
Aaron129Polocraft <aarongaymeryt12@gmail.com>
2020-06-18 19:58:53 -0400

    Actualización slap.js
    Añadidas mas imágenes al código

--- commands/slap.js
+++ commands/slap.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     let user = message.mentions.users.first() || (message.guild.members.cache.get(args[0]) !== undefined ? message.guild.members.cache.get(args[0]).user : false);
     if (!user) return new_error(message, "Ocurrió un error", "Debes mencionar a un usuario o especificar su ID."); // hay que cambiar esto por algo más original
 
-    const images = ["https://cdn.discordapp.com/attachments/720857177107202079/720857497518342194/slap1.gif", "https://cdn.discordapp.com/attachments/720857177107202079/720857537141932142/slap2.gif", "https://cdn.discordapp.com/attachments/720857177107202079/720857869947371610/slap3.gif"] // etc, etc, etc
+    const images = ["https://cdn.discordapp.com/attachments/720857177107202079/720857497518342194/slap1.gif", "https://cdn.discordapp.com/attachments/720857177107202079/720857537141932142/slap2.gif", "https://cdn.discordapp.com/attachments/720857177107202079/720857869947371610/slap3.gif", "https://cdn.discordapp.com/attachments/720857177107202079/723325183267700736/slap4.gif", "https://cdn.discordapp.com/attachments/720857177107202079/723325457713332265/slap6.gif", "https://cdn.discordapp.com/attachments/720857177107202079/723325735170867280/slap7.gif"] // etc, etc, etc
     const phrases = ["{author} le ha metido una cachetada a {user}", "A {author} le pego una arrechera y le volteo la jeta a {user}"] // etc...
     const randomImg = images[Math.floor(Math.random() * images.length)];
     const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
